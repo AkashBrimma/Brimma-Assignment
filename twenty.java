56734e4cba39340cb95270a6512b21537cb6a408
AkashBrimma <109518872+AkashBrimma@users.noreply.github.com>
2022-07-20 12:00:17 +0530

    Update twenty.java

--- twenty.java
+++ twenty.java
@@ -1,14 +1,40 @@
-Public class Twenty{
-    public static void main(String [] args){
-        long fac=1;
-        for(long i=10; i>1;i-- ){
-            fac*=i;
+import java.math.BigInteger;
+public class Twenty {
+    public static void main(String[] args) {
+        int n = 100;
+        if (args.length == 1) {
+            try {
+                n = Integer.parseInt(args[0]);
+            } catch (Exception e) {
+                System.out.println("arg parse error. will use default n=" + n
+                        + "\n\n");
+            }
         }
-        int sum=0;
-        while(fac>0){
-            sum+=(fac % 10);
-            fac/=10;
+        Twenty number = new Twenty();
+        number.sumDigits(number.getFactorial(n).toString());
+
+    }
+
+    public BigInteger getFactorial(int n) {
+        if (n < 0) {
+            return BigInteger.ZERO;
+        } else if (n > 0) {
+            BigInteger factorial = BigInteger.ONE;
+            for (int i = 1; i <= n; i++) {
+                factorial = factorial.multiply(BigInteger.valueOf(i));
+            }
+            return factorial;
+        } else {
+            return BigInteger.ONE;
         }
-        System.out.println(sum);
+    }
+
+    public void sumDigits(String s) {
+        long sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            sum += Long.parseLong(s.substring(i, i + 1));
+            System.out.print(s.charAt(i));
+        }
+        System.out.println("\n"+sum);
     }
 }
