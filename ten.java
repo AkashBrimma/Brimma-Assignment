22e2e5629ebd5e7649fb8230b4ffb444eb925a2e
AkashBrimma <109518872+AkashBrimma@users.noreply.github.com>
2022-07-20 12:26:16 +0530

    Create ten.java

---
+++ ten.java
@@ -0,0 +1,28 @@
+public class Ten {
+    public static void main(String [] args){
+        Ten number = new Ten();
+        long Sum = 0;
+        int i = 1;
+        while (i < 2000000) {
+            if (number.isPrime(i)) {
+                Sum += i;
+            }
+            i += 2;
+        }
+        System.out.println(Sum + 2);
+    }
+    public boolean isPrime(int n)
+    {
+        if (n == 1){
+            return false;
+        }
+        int k = (int) Math.sqrt(n);
+        for (int i = 2; i <= k; i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+}
