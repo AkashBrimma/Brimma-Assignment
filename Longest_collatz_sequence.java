08c6d389d28e004f78656424d782aae8879a96ab
AkashBrimma <109518872+AkashBrimma@users.noreply.github.com>
2022-07-31 20:48:59 +0530

    Update and rename fourteen.java to Longest_collatz_sequence.java

--- Longest_collatz_sequence.java
+++ Longest_collatz_sequence.java
@@ -1,8 +1,20 @@
+/*14. The following iterative sequence is defined for the set of positive integers:
+        n → n/2 (n is even)
+        n → 3n + 1 (n is odd)
+        Using the rule above and starting with 13, we generate the following sequence:
+        13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+        It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms.
+        Although it has not been proved yet (Collatz Problem), it is thought that all starting
+        numbers finish at 1.
+        Which starting number, under one million, produces the longest chain?
+        Ans: 837799*/
+
+
 import java.util.ArrayList;
 import java.util.List;
-public class fourteen {
+public class Longest_collatz_sequence {
     public static void main(String[] args) {
-        int limit = 1000000; 
+        int limit = 1000000;
         int size = 0, max = 1, j = 0;
         for (int i = 2; i < limit; i++) {
             size = getChainSize(i);
@@ -14,7 +26,6 @@ public class fourteen {
         System.out.println( j);
     }
     private static int getChainSize(int n) {
-
         long num = n;
         List<Long> list = new ArrayList<>();
         int size = 0;
@@ -29,5 +40,4 @@ public class fourteen {
         size = list.size();
         return size;
     }
-
 }
