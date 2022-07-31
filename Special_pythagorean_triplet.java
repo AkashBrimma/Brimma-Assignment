498dc696667f8880734c1bdaf1591440275e1ef6
AkashBrimma <109518872+AkashBrimma@users.noreply.github.com>
2022-07-31 20:52:45 +0530

    Update and rename nine.java to  Special_pythagorean_triplet.java

--- Special_pythagorean_triplet.java
+++ Special_pythagorean_triplet.java
@@ -1,4 +1,11 @@
-public class nine {
+/*9. A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+        a2 + b2 = c2
+        For example, 32 + 42 = 9 + 16 = 25 = 52.
+        There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+        Find the product abc.
+        Ans: 31875000*/
+
+public class Special_pythagorean_triplet {
     public static void main(String[] args) {
         int sum = 1000;
         for (int a = 2; a < sum; a++) {
