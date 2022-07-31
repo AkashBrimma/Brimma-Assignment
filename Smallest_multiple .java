06dc3100d2eba27621b802982cf807154f01decc
AkashBrimma <109518872+AkashBrimma@users.noreply.github.com>
2022-07-31 20:42:03 +0530

    Update and rename five.java to Smallest_multiple .java

--- Smallest_multiple .java
+++ Smallest_multiple .java
@@ -1,4 +1,10 @@
-public class five {
+/*5. 2520 is the smallest number that can be divided by each of the numbers from 1 to 10
+        without any remainder.
+        What is the smallest positive number that is evenly divisible by all of the numbers
+        from 1 to 20?
+        Ans: 232792560*/
+
+public class Smallest_multiple {
     public static void main(String[] args) {
         final int start = 3, step = 20;
         for (int i = step;; i += step) {
